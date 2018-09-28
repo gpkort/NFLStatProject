3410ae892a125a70f94596b0dccccd86a2b142fc
gregk <gpkort@gmail.com>
2018-09-28 14:18:20 -0600

    finished to all players

--- EveryPlayer.py
+++ EveryPlayer.py
@@ -22,9 +22,11 @@ def get_player_summary(url: str):
     req = requests.get(url)
     soup = BeautifulSoup(req.text, "lxml")
     body = soup.find_all('table', {'class': 'row_summable sortable stats_table'})[0].find('tbody')
+    playername = soup.find('h1', {'itemprop': 'name'}).text
     rows = body.find_all('tr', {'class': 'full_table'})
 
     totalgames, gamestart = 0, 0
+    teams, pos = str(), str()
 
     for row in rows:
         for g in row.find_all('td', {'data-stat': 'g'}):
@@ -33,14 +35,30 @@ def get_player_summary(url: str):
         for gs in row.find_all('td', {'data-stat': 'gs'}):
             if len(gs.text) > 0:
                 gamestart = gamestart + int(gs.text)
+        for team in row.find_all('td', {'data-stat': 'team'}):
+            if len(team.text) > 0 and team.text not in teams:
+                teams = teams + team.text + ','
+        for p in row.find_all('td', {'data-stat': 'pos'}):
+            if len(p.text) > 0 and p.text not in pos:
+                pos = p.text + ','
 
-    print(totalgames)
-    print(gamestart)
+    yr = rows[0].find_all('th', {'data-stat': 'year_id'})
+    startyear = int(yr[0].text) if len(yr[0].text) > 0 else None
+    yr = rows[-1].find_all('th', {'data-stat': 'year_id'})
+    stopyear = int(yr[0].text) if len(yr[0].text) > 0 else None
 
-
+    return {
+        'name': playername,
+        'position':(pos[-1] if len(pos) > 0 else ''),
+        'start_year': startyear,
+        'stop_year': stopyear,
+        'total_game': totalgames,
+        'games_started': gamestart
+        }
 
 if __name__ == '__main__':
-    get_player_summary(URL + '/players/A/AaitIs00.htm')
+    info = get_player_summary(URL + '/players/A/AaitIs00.htm')
+    print(info)
     # all = []
     # for i in range(65, 91):
     #     all.extend(get_players_by_letter(chr(i)))
