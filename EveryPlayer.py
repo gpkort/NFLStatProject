a74d4d5640903226be82ca02fd90e33592313724
gregk <gpkort@gmail.com>
2018-10-04 14:15:36 -0600

    refined all player

--- EveryPlayer.py
+++ EveryPlayer.py
@@ -6,7 +6,10 @@ import collections
 
 URL = "https://www.pro-football-reference.com"
 PLAYER = "players"
+DELIMTER = '|'
 
+def get_year_int(yr: str):
+    return int(yr.replace('*', '').replace('+', '')) if len(yr) > 0 else None
 
 def get_players_by_letter(letter: str):
     url = URL + '/' + PLAYER + '/' + letter
@@ -26,8 +29,9 @@ def get_player_summary(url: str):
     tables = soup.find_all('table', {'class': 'row_summable sortable stats_table'})
 
     if len(tables) == 0:
-        tables = soup.find_all('table', {'class': 'sortable stats_table'})
+        tables = soup.find_all('table', {'class': 'sortable stats_table'}) # row_summable sortable stats_table
 
+    # print('There are {} tables'.format({len(tables)}))
     if len(tables) == 0:
         raise Exception("Cannot find Table")
 
@@ -39,8 +43,18 @@ def get_player_summary(url: str):
         rows = body.find_all('tr', {'class': 'full_table'})
 
     if len(rows) == 0:
+        tables = soup.find_all('table', {'id': 'games_played'})
+        body = tables[0].find('tbody')
+        rows = body.find_all('tr', {'class': 'full_table'})
+
+
+    if len(rows) == 0:
         raise Exception('Could not find rows in table')
 
+    return process_rows(rows, playername)
+
+
+def process_rows(rows, playername):
     totalgames, gamestart = 0, 0
     teams, pos = str(), str()
 
@@ -53,17 +67,17 @@ def get_player_summary(url: str):
                 gamestart = gamestart + int(gs.text)
         for team in row.find_all('td', {'data-stat': 'team'}):
             if len(team.text) > 0 and team.text not in teams:
-                teams = teams + team.text + ','
+                teams = teams + team.text + DELIMTER
         for p in row.find_all('td', {'data-stat': 'pos'}):
             if len(p.text) > 0 and p.text not in pos:
-                pos = p.text + ','
+                pos = p.text + DELIMTER
 
     yr = rows[0].find_all('th', {'data-stat': 'year_id'})
-    startyear = int(yr[0].text.replace('*', '').replace('+', '')) if len(yr[0].text) > 0 else None
+    startyear = get_year_int(yr[0].text)
 
     if len(rows) > 1:
         yr = rows[-1].find_all('th', {'data-stat': 'year_id'})
-        stopyear = int(yr[0].text.replace('*', '').replace('+', '')) if len(yr[0].text) > 0 else None
+        stopyear = get_year_int(yr[0].text)
     else:
         stopyear = startyear
 
@@ -119,6 +133,7 @@ def make_every_player_file(player_url: str, out_file: str):
 
 
 if __name__ == '__main__':
-    # print(get_player_summary('https://www.pro-football-reference.com/players/A/AdamDa01.htm'))
-    make_every_player_file('wrongtable.txt', 'ErrTable.log')
+    # print(get_player_summary('https://www.pro-football-reference.com/players/A/AdamMa00.htm'))
+    print(get_player_summary('https://www.pro-football-reference.com/players/E/ElwaJo00.htm'))
+    # make_every_player_file('wrongtable.txt', 'ErrTable.log')
     # print('1975*+'.replace('*', '').replace('+', ''))
